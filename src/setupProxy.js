4c279c85fa6776cc5e906fe2821022fdce1f7f2e
Ashley Baker <ashleybm@umich.edu>
2025-02-17 23:56:09 -0500

    Remove setupProxy.js

--- src/setupProxy.js
+++
@@ -1,8 +0,0 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-const path = require('path');
-const express = require('express');
-
-module.exports = function(app) {
-  // Serve the data directory
-  app.use('/data', express.static(path.join(__dirname, '../data')));
-};
